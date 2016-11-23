11f662b9b430a1ac5b246bbcde20108197043368
5000 <5000@hb5000>
2016-11-23 15:37:17 +0900

    String 연습

---
+++ src/com/hanbit/ex01/StringExercise.java
@@ -0,0 +1,13 @@
+package com.hanbit.ex01;
+
+public class StringExercise {
+
+	public static void main(String[] args) {
+		
+		// 1. 입력받은 문자열의 길이를 구해서 출력하시오.
+		String name = "Hanbit";
+		int lengthOfName = getLengthOfString(name); 
+		System.out.println(name + " : " + lengthOfName);
+	}
+	
+}
