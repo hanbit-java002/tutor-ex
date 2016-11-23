957fd73fc17d50533f026b4631bb1c25d684b1e6
5000 <5000@hb5000>
2016-11-23 15:49:27 +0900

    문자열 길이 검사

--- src/com/hanbit/ex01/StringExercise.java
+++ src/com/hanbit/ex01/StringExercise.java
@@ -5,9 +5,21 @@ public class StringExercise {
 	public static void main(String[] args) {
 		
 		// 1. 입력받은 문자열의 길이를 구해서 출력하시오.
-		String name = "Hanbit";
+		String name = "한빛";
 		int lengthOfName = getLengthOfString(name); 
 		System.out.println(name + " : " + lengthOfName);
+		
+		// 2. 입력받은 문자열의 길이가 10개 이상이면
+		//    "길이가 너무 길어요" 를 출력하세요.
+		boolean validLength = isShorterThan10("안녕하세요.");
+		if (!validLength) {
+			System.out.println("길이가 너무 길어요");
+		}
+	}
+
+	static int getLengthOfString(String str) {
+
+		return str.length();
 	}
 	
 }
