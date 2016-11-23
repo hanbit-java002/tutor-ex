0aafe0e0ea65b80a5409f2150aec4b3b805e2a03
5000 <5000@hb5000>
2016-11-23 12:34:59 +0900

    영문 월명을 출력 문제 추가

--- src/com/hanbit/ex01/StudyFunctions.java
+++ src/com/hanbit/ex01/StudyFunctions.java
@@ -6,6 +6,16 @@ public class StudyFunctions {
 		
 		// 1. 변수로 받은 문자열을 출력하는 함수를 작성하세요.
 		printString("Hello");
+
+		// 2. 변수로 받은 월에 따라 영문 월명을 출력하세요.
+		//   ex: month = 1 -> January
+		//   ex: month = 2 -> Febrary
+		int month = 1;
+		printMonthName(month);
+	}
+
+	static void printString(String string) {
+		System.out.println(string);
 	}
 	
 }
