a23032afcd75a9c9b35c758cf218ffeaba838e93
5000 <5000@hb5000>
2016-11-23 16:06:46 +0900

    유효한 길이의 문자열인지 판단하는 함수

--- src/com/hanbit/ex01/StringExercise.java
+++ src/com/hanbit/ex01/StringExercise.java
@@ -11,10 +11,31 @@ public class StringExercise {
 		
 		// 2. 입력받은 문자열의 길이가 10개 이상이면
 		//    "길이가 너무 길어요" 를 출력하세요.
-		boolean validLength = isShorterThan10("안녕하세요.");
+		boolean validLength = isShorterThan10("안녕하세요. 한빛입니다.");
 		if (!validLength) {
 			System.out.println("길이가 너무 길어요");
 		}
+		else {
+			System.out.println("유효합니다");
+		}
+		
+		// 3. 문자열과 최소길이, 최대길이를 입력받아
+		//    유효한 길이의 문자열인지 판단하는 함수를 작성하세요.
+		String str = "자바 참 쉬워요.";
+		int minLength = 4;
+		int maxLength = 12;
+		boolean validString = validateStringLength(str, minLength, maxLength);
+		if (validString) {
+			System.out.println("유효한 스트링");
+		}
+		else {
+			System.out.println("유효하지 않은 스트링");
+		}
+	}
+	
+	static boolean isShorterThan10(String str) {
+		
+		return getLengthOfString(str) < 10;
 	}
 
 	static int getLengthOfString(String str) {
