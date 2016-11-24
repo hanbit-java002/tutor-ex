b211c54321d5f0e4310d64b2c4434365f75bdc70
5000 <5000@hb5000>
2016-11-24 10:55:09 +0900

    첫번째 수 부터 두번째 수까지의 합

--- src/com/hanbit/ex01/ForExercise.java
+++ src/com/hanbit/ex01/ForExercise.java
@@ -7,6 +7,22 @@ public class ForExercise {
 		// 1. 1부터 10까지의 합을 구하는 함수를 작성하세요.
 		System.out.println(getSumOneToTen());
 		
+		// 2. 두개의 정수를 입력받아
+		//    첫번째 수 부터 두번째 수까지의 합을
+		//    구하는 함수를 작성하세요.
+		int from = 10, to = 30;
+		System.out.println(getSum(from, to));
+		
+	}
+
+	static int getSumOneToTen() {
+		int sum = 0;
+		
+		for (int i=1;i<=10;i++) {
+			sum += i;
+		}
+		
+		return sum;
 	}
 	
 }
