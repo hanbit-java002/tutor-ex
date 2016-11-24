1ecb045b367f79a591a5ea9776a68c98e0c34e9c
5000 <5000@hb5000>
2016-11-24 12:31:06 +0900

    시작월부터 12월까지의 월명을 배열로 반환

---
+++ src/com/hanbit/ex01/ForAndArray.java
@@ -0,0 +1,14 @@
+package com.hanbit.ex01;
+
+public class ForAndArray {
+
+	public static void main(String[] args) {
+	
+		// 1. 시작월부터 12월까지의 월명을 배열로 반환하세요.
+		//  ex: startMonth = 3 -> {"3월", "4월", ... , "12월"}
+		int startMonth = 3;
+		String[] monthNames = getMonthNames(startMonth);
+		
+	}
+	
+}
