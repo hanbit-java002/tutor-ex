4d3d7e7ef4b8f03a5d7b5f9eb7e3c5d09628e220
5000 <5000@hb5000>
2016-11-24 10:32:00 +0900

    1부터 10까지의 합

---
+++ src/com/hanbit/ex01/ForExercise.java
@@ -0,0 +1,12 @@
+package com.hanbit.ex01;
+
+public class ForExercise {
+
+	public static void main(String[] args) {
+	
+		// 1. 1부터 10까지의 합을 구하는 함수를 작성하세요.
+		System.out.println(getSumOneToTen());
+		
+	}
+	
+}
