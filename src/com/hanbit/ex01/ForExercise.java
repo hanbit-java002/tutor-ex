910f28966102008fb6964f0b0cfb50c8f8e0fd3f
5000 <5000@hb5000>
2016-11-24 11:14:24 +0900

    첫번째 수 부터 두번째 수까지 중 짝수의 합

--- src/com/hanbit/ex01/ForExercise.java
+++ src/com/hanbit/ex01/ForExercise.java
@@ -10,9 +10,24 @@ public class ForExercise {
 		// 2. 두개의 정수를 입력받아
 		//    첫번째 수 부터 두번째 수까지의 합을
 		//    구하는 함수를 작성하세요.
-		int from = 10, to = 30;
+		int from = 1, to = 10;
 		System.out.println(getSum(from, to));
 		
+		// 3. 두개의 정수를 입력받아
+		//    첫번째 수 부터 두번째 수까지 중 짝수의 합을
+		//    구하는 함수를 작성하세요.
+		System.out.println(getSumOnlyEven(from, to));
+		
+	}
+
+	static int getSum(int from, int to) {
+		int sum = 0;
+		
+		for (int i=from;i<=to;i++) {
+			sum += i;
+		}
+		
+		return sum;
 	}
 
 	static int getSumOneToTen() {
