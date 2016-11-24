4ea94e560738f7c5fd2ea7aab46a704f6f9d90a8
5000 <5000@hb5000>
2016-11-24 16:09:48 +0900

    월명 배열의 각 글자 수를 배열로

--- src/com/hanbit/ex01/ArrayExercise.java
+++ src/com/hanbit/ex01/ArrayExercise.java
@@ -8,7 +8,43 @@ public class ArrayExercise {
 		//    배열을 만들어 반환하세요.
 		//    단, {"January", ... "December"} 문법 제외
 		String[] monthNames = createMonthNameArray();
-		printArray(monthNames);		
+		printArray(monthNames);
+		
+		// 2. 월명 배열의 각 글자 수를 배열로 만들어 반환하세요.
+		//  hint: int lengthOfString = "March".length();
+		int[] monthNamesLength = countMonthNamesLength(monthNames);
+		printArray(monthNamesLength);
+	}
+	
+	static String[] createMonthNameArray() {
+		String[] monthNames = new String[12];
+		
+		monthNames[0] = "January";
+		monthNames[1] = "February";
+		monthNames[2] = "March";
+		monthNames[3] = "April";
+		monthNames[4] = "May";
+		monthNames[5] = "June";
+		monthNames[6] = "July";
+		monthNames[7] = "August";
+		monthNames[8] = "September";
+		monthNames[9] = "October";
+		monthNames[10] = "November";
+		monthNames[11] = "December";
+		
+		return monthNames;
+	}
+
+	static void printArray(int[] array) {
+		System.out.print("[");
+		for (int i=0;i<array.length;i++) {
+			if (i > 0) {
+				System.out.print(", ");
+			}
+			
+			System.out.print(array[i]);
+		}
+		System.out.println("]");
 	}
 	
 	static void printArray(String[] array) {
