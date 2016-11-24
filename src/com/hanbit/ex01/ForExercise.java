ee4236a456797bf2e7f5ad773d2b2e15f0c41d74
5000 <5000@hb5000>
2016-11-24 11:33:41 +0900

    복합 Sum

--- src/com/hanbit/ex01/ForExercise.java
+++ src/com/hanbit/ex01/ForExercise.java
@@ -10,7 +10,7 @@ public class ForExercise {
 		// 2. 두개의 정수를 입력받아
 		//    첫번째 수 부터 두번째 수까지의 합을
 		//    구하는 함수를 작성하세요.
-		int from = 1, to = 10;
+		int from = 5, to = 110;
 		System.out.println(getSum(from, to));
 		
 		// 3. 두개의 정수를 입력받아
@@ -18,6 +18,25 @@ public class ForExercise {
 		//    구하는 함수를 작성하세요.
 		System.out.println(getSumOnlyEven(from, to));
 		
+		// 4. 두개의 정수를 입력받아
+		//    두수의 합이 홀수이면
+		//    첫번째 수 부터 두번째 수까지의 합을 구하고
+		//    두수의 합이 짝수이면
+		//    두번째 수를 반환하는 함수를 작성하세요.
+		System.out.println(getSumComplex(from, to));
+		
+	}
+
+	static int getSumOnlyEven(int from, int to) {
+		int sum = 0;
+		
+		for (int i=from;i<=to;i++) {
+			if (i % 2 == 0) {
+				sum += i;
+			}
+		}
+		
+		return sum;
 	}
 
 	static int getSum(int from, int to) {
