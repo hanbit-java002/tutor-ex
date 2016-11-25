e1cbc5c77e961ae849c98582a2f45ebbb49a6f4f
5000 <5000@hb5000>
2016-11-25 15:19:46 +0900

    calculateMore

--- src/com/hanbit/ex01/Calculator.java
+++ src/com/hanbit/ex01/Calculator.java
@@ -31,6 +31,20 @@ public class Calculator {
 		
 		return 0;
 	}
+	
+	// 함수명: calculateMore
+	// 매개변수1: int x
+	// 매개변수2: int y
+	// 매개변수3: String operator1 ("+", "-", "*", "/", "%")
+	// 매개변수4: String operator2
+	// 매개변수5: String operator3
+	// 리턴타입: int
+	// 설명: 두개의 정수를 첫번째 연산자와 세번째 연산자로 계산한 후
+	//       각 결과를 두번째 연산자로 계산하여 결과를 반환합니다.
+	// 예: calculateMore(3, 4, "*", "+", "-") -> 11
+	
+	
+	
 
 	// sum(더하기), subtract(빼기), multiply(곱하기)
 	// divide(나누기), remain(나머지) 함수를 만들고
