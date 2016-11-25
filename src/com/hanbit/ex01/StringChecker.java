f1f9af15fdc7c9129a4a201d126bdb56db5a7176
5000 <5000@hb5000>
2016-11-25 17:29:27 +0900

    예문 추가

--- src/com/hanbit/ex01/StringChecker.java
+++ src/com/hanbit/ex01/StringChecker.java
@@ -9,6 +9,8 @@ public class StringChecker {
 	 * 설명: 문자열을 입력받아 숫자를 포함하고 있는지 여부를 반환합니다.
 	 * 힌트1: char[] chars = str.toCharArray();
 	 * 힌트2: if (chars[i] == '3') { ... }
+	 * 예1: hasNumber("hello123") -> true
+	 * 예2: hasNumber("박재홍") -> false
 	 */
 	
 	
