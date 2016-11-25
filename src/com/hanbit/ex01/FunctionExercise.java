7d8186b3533433dbf64b8e15cd2e6151b7c647f0
5000 <5000@hb5000>
2016-11-25 11:12:07 +0900

    함수만들기 연습

---
+++ src/com/hanbit/ex01/FunctionExercise.java
@@ -0,0 +1,127 @@
+package com.hanbit.ex01;
+
+public class FunctionExercise {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	/*
+	 * 함수명: getFullName
+	 * 매개변수1 : String firstName
+	 * 매개변수2 : String lastName
+	 * 리턴타입 : String
+	 */
+	static String getFullName(String firstName, String lastName) {
+		
+		return null;
+	}
+	
+	/*
+	 * 함수명: plusThreeNumbers
+	 * 매개변수1: int first
+	 * 매개변수2: int second
+	 * 매개변수3: int third
+	 * 리턴타입: int
+	 */
+	static int plusThreeNumbers(int first, int second, int third) {
+		
+		return 0;
+	}
+	
+	/*
+	 * 함수명: isTrue
+	 * 리턴타입: boolean
+	 */
+	static boolean isTrue() {
+		
+		return false;
+	}
+	
+	/*
+	 * 함수명: printName
+	 * 리턴타입: 없음
+	 */
+	static void printName() {
+		
+	}
+	
+	/*
+	 * 함수명: logError
+	 * 매개변수: String message
+	 * 리턴타입: 없음
+	 */
+	static void logError(String message) {
+		
+	}
+	
+	/*
+	 * 함수명: caculate
+	 * 매개변수1: int left
+	 * 매개변수2: String operator
+	 * 매개변수3: int right
+	 * 리턴타입: long
+	 */
+	static long caculate(int left, String operator, int right) {
+		
+		return 0;
+	}
+	
+	/*
+	 * 함수명: validatePassword
+	 * 매개변수1: String password
+	 * 매개변수2: int maxLength
+	 * 매개변수3: int minLength
+	 * 매개변수4: char[] allowedCharacters
+	 * 리턴타입: boolean
+	 */
+	static boolean validatePassword(String password, int maxLength, int minLength,
+			char[] allowedCharacters) {
+		
+		return false;
+	}
+	
+	/*
+	 * 함수명: splitNames
+	 * 매개변수1: String names
+	 * 매개변수2: String separator
+	 * 리턴타입: String[]
+	 */
+	static String[] splitnames(String names, String separator) {
+		
+		return null;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
