f99946caff356480c776a24d9df8c0b392084844
5000 <5000@hb5000>
2016-11-28 16:03:57 +0900

    lastIndexOf 문제

--- src/com/hanbit/ex01/StringChecker.java
+++ src/com/hanbit/ex01/StringChecker.java
@@ -150,8 +150,17 @@ public class StringChecker {
 	 * 매개변수1: String str
 	 * 매개변수2: char ch
 	 * 리턴타입: int
+	 * 설명: 문자열과 문자 1개를 입력받아 해당 문자의 위치를
+	 *       뒤에서 부터 카운트하여 반환합니다.
+	 *       단, 해당 문자가 없으면 -1 을 반환합니다.
+	 * 예1: indexOf("hello", 'l') -> 3
+	 * 예2: indexOf("hello", 'o') -> 4
+	 * 예3: indexOf("christmas", 'd') -> -1
 	 */
-	
+	static int lastIndexOf(String str, char ch) {
+		
+		return 0;
+	}
 	
 	
 	
