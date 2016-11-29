839e601c69eb902cf82078a34cc1b2fc7649a3b5
5000 <5000@hb5000>
2016-11-29 10:12:22 +0900

    compare

---
+++ src/com/hanbit/ex01/NumberUtils.java
@@ -0,0 +1,26 @@
+package com.hanbit.ex01;
+
+public class NumberUtils {
+
+	public static void main(String[] args) {
+		int x = 8;
+		int y = 10;
+		
+		// compare를 사용하여 결과값을 출력하세요.
+		// 출력: compare(x, y)의 결과는 ? 입니다.
+		
+	}
+	
+	/*
+	 * 함수명: compare
+	 * 매개변수: int left
+	 * 매개변수: int right
+	 * 리턴타입: int
+	 * 설명: 두개의 정수를 입력받아 왼쪽정수가 크면 1
+	 *       오른쪽 정수가 크면 -1, 같으면 0을 반환합니다.
+	 * 예1: compare(1, 4) -> -1
+	 * 예2: compare(50, 4) -> 1
+	 * 예3: compare(7, 7) -> 0
+	 */
+	
+}
