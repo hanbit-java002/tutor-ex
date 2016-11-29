3c06e6bb0c8e244bb2e1810b64bc75c8ca3c6f5e
5000 <5000@hb5000>
2016-11-29 15:03:24 +0900

    reverse

---
+++ src/com/hanbit/ex01/StringUtils.java
@@ -0,0 +1,22 @@
+package com.hanbit.ex01;
+
+public class StringUtils {
+
+	public static void main(String[] args) {
+		// reverse 함수를 사용하여 다음 문자열을 뒤집어 출력하세요.
+		String fruit = "banana";
+		
+	}
+	
+	/*
+	 * 함수명: reverse
+	 * 매개변수: String str
+	 * 리턴타입: String
+	 * 설명: 입력받은 문자열을 뒤집어 반환합니다.
+	 *       단, 문자열이 null 이면 null을 반환합니다.
+	 * 예1: reverse("hello") -> "olleh"
+	 * 예2: reverse(null) -> null
+	 * 예3: reverse("") -> ""
+	 */
+	
+}
