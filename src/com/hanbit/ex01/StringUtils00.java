b0bdbcb0c50851e9012d5820a5b1c9f4e0aedab8
5000 <5000@hb5000>
2016-11-30 10:59:12 +0900

    compare

--- src/com/hanbit/ex01/StringUtils00.java
+++ src/com/hanbit/ex01/StringUtils00.java
@@ -3,7 +3,7 @@ package com.hanbit.ex01;
 public class StringUtils00 {
 
 	public static void main(String[] args) {
-		
+		System.out.println(compare("백미리", "백수현"));
 	}
 	
 	/*
@@ -13,6 +13,41 @@ public class StringUtils00 {
 	 * 리턴타입: int
 	 * 설명: 두개의 문자열을 입력받아 왼쪽이 크면 1,
 	 *       오른쪽이 크면 -1, 같으면 0을 반환합니다.
+	 * 예1: compare("11", "2") -> -1
+	 * 예2: compare("11111111111111", "2") -> -1
+	 * 예3: compare("abc", "abcd") -> -1
 	 */
-	
+	static int compare(String left, String right) {
+		if (left == null && right == null) {
+			return 0;
+		}
+		else if (left != null && right == null) {
+			return 1;
+		}
+		else if (left == null && right != null) {
+			return -1;
+		}
+		
+		char[] leftChars = left.toCharArray();
+		char[] rightChars = right.toCharArray();
+		
+		for (int i=0;i<leftChars.length;i++) {
+			if (i >= rightChars.length) {
+				return 1;
+			}
+			
+			if (leftChars[i] > rightChars[i]) {
+				return 1;
+			}
+			else if (leftChars[i] < rightChars[i]) {
+				return -1;
+			}
+		}
+		
+		if (leftChars.length < rightChars.length) {
+			return -1;
+		}
+		
+		return 0;
+	}
 }
