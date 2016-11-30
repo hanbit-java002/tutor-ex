153d997718f856ada68a07f3f62b5197c484f37d
5000 <5000@hb5000>
2016-11-30 10:40:10 +0900

    compare

---
+++ src/com/hanbit/ex01/StringUtils00.java
@@ -0,0 +1,18 @@
+package com.hanbit.ex01;
+
+public class StringUtils00 {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	/*
+	 * 함수명: compare
+	 * 매개변수1: String left
+	 * 매개변수2: String right
+	 * 리턴타입: int
+	 * 설명: 두개의 문자열을 입력받아 왼쪽이 크면 1,
+	 *       오른쪽이 크면 -1, 같으면 0을 반환합니다.
+	 */
+	
+}
