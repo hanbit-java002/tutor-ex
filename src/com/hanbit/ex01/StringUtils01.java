61f4af67aac6ce11eb45e975ec2eaf9fc18130ed
5000 <5000@hb5000>
2016-12-01 11:40:00 +0900

    trim, leftTrim, removeWhitespace

--- src/com/hanbit/ex01/StringUtils01.java
+++ src/com/hanbit/ex01/StringUtils01.java
@@ -11,5 +11,81 @@ public class StringUtils01 {
 	 * 예2: trim("a b ") -> "a b"
 	 * 예3: trim(null) -> null
 	 */
+	static String trim(final String str) {
+		if (str == null) {
+			return null;
+		}
+		
+		char[] chars = str.toCharArray();
+		String result = "";
+		
+		for (int i=0;i<chars.length;i++) {
+			char ch = chars[i];
+			
+			if (result.length() == 0 && StringUtils.isWhitespace(ch)) {
+				continue;
+			}
+			
+			result += ch;
+		}
+		
+		chars = result.toCharArray();
+		result = "";
+		
+		for(int i=0;i<chars.length;i++) {
+			char ch = chars[chars.length - 1 - i];
+			
+			if (result.length() == 0 && StringUtils.isWhitespace(ch)) {
+				continue;
+			}
+			
+			result = ch + result;
+		}
+		
+		return result;
+	}
+	
+	static String rightTrim(final String str) {
+		
+		return null;
+	}
+	
+	static String leftTrim(final String str) {
+		if (str == null) {
+			return null;
+		}
+		
+		char[] chars = str.toCharArray();
+		String result = "";
+		
+		for (int i=0;i<chars.length;i++) {
+			if (result.length() == 0 && StringUtils.isWhitespace(chars[i])) {
+				continue;
+			}
+			
+			result += chars[i];
+		}
+		
+		return result;
+	}
+	
+	static String removeWhitespace(final String str) {
+		if (str == null) {
+			return null;
+		}
+		
+		char[] chars = str.toCharArray();
+		String result = "";
+		
+		for (int i=0;i<chars.length;i++) {
+			if (StringUtils.isWhitespace(chars[i])) {
+				continue;
+			}
+			
+			result += chars[i];
+		}
+		
+		return result;
+	}
 	
 }
---
+++ src/com/hanbit/ex01/Tester.java
@@ -0,0 +1,12 @@
+package com.hanbit.ex01;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		
+		System.out.println("[" + StringUtils01.trim("   a  b  c   ") + "]");
+		System.out.println("[" + StringUtils01.leftTrim("   a  b  c   ") + "]");
+		System.out.println("[" + StringUtils01.removeWhitespace("   a  b  c   ") + "]");
+	}
+	
+}
