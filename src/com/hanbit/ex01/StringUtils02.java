0ea76ab21f09a7526e9865994e491c6af7c34f82
5000 <5000@hb5000>
2016-12-01 15:40:23 +0900

    substring overloading

--- src/com/hanbit/ex01/StringUtils02.java
+++ src/com/hanbit/ex01/StringUtils02.java
@@ -16,8 +16,53 @@ public class StringUtils02 {
 	 * @return
 	 */
 	static String substring(String str, int beginIndex, int endIndex) {
+		if (str == null || beginIndex < 0 || endIndex < 0) {
+			return null;
+		}
+		
+		if (beginIndex >= str.length() || beginIndex >= endIndex) {
+			return "";
+		}
+		
+		endIndex = endIndex > str.length() ? str.length() : endIndex; 
+		
+		char[] chars = str.toCharArray();
+		String result = "";
+		
+		for (int i=beginIndex;i<endIndex;i++) {
+			result += chars[i];
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * 주어진 문자열에서 시작인덱스부터의 문자열을 반환합니다.
+	 * 
+	 * 예1: substring("hello", 1) -> "ello"
+	 * 예2: substring("hello", 3) -> "lo"
+	 * 예3: substring("hello", 5) -> ""
+	 * 예4: substring("hello", -1) -> null
+	 * 
+	 * @param str
+	 * @param beginIndex
+	 * @return
+	 */
+	static String substring(String str, int beginIndex) {
 		
 		return null;
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
