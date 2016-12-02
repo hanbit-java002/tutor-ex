7c221a9a825034d26b3534076acd8e98f4088c65
5000 <5000@hb5000>
2016-12-02 09:36:24 +0900

    split

---
+++ src/com/hanbit/ex01/StringUtils03.java
@@ -0,0 +1,22 @@
+package com.hanbit.ex01;
+
+public class StringUtils03 {
+
+	/**
+	 * 입력받은 문자열을 구분자를 기준으로 분리한 문자열의
+	 * 배열을 반환합니다.
+	 * 
+	 * 예1: split("hello, world", ',') -> ["hello", " world"]
+	 * 예2: split("5,4,3,2,1", ',') -> ["5", "4", "3", "2", "1"]
+	 * 예3: split("이상윤|조영욱", '|') -> ["이상윤", "조영욱"]
+	 * 
+	 * @param str
+	 * @param separator 구분자
+	 * @return
+	 */
+	static String[] split(String str, char separator) {
+		
+		return null;
+	}
+	
+}
