1a46567b4a4f1c2b93f01d345ff5489facce7f37
5000 <5000@hb5000>
2016-12-02 11:33:08 +0900

    split overloading

--- src/com/hanbit/ex01/StringUtils03.java
+++ src/com/hanbit/ex01/StringUtils03.java
@@ -113,6 +113,22 @@ public class StringUtils03 {
 		return result;
 	}
 	
+	/**
+	 * 입력받은 문자열을 구분자를 기준으로 분리한 문자열의
+	 * 배열을 반환합니다.
+	 * 
+	 * 예1: split("hello, world", ",") -> ["hello", " world"]
+	 * 예2: split("5,4,3,2,1", ",") -> ["5", "4", "3", "2", "1"]
+	 * 예3: split("이상윤||조영욱|||", "||") -> ["이상윤", "조영욱", "|"]
+	 * 
+	 * @param str
+	 * @param separator 구분자
+	 * @return
+	 */
+	static String[] split(String str, String separator) {
+		
+		return null;
+	}
 	
 	
 	
