f4a26312c9fc27498de1e018f96fdb2a749506f5
5000 <5000@hb5000>
2016-12-02 12:42:04 +0900

    splitNotBlankAndTrim

--- src/com/hanbit/ex01/StringUtils03.java
+++ src/com/hanbit/ex01/StringUtils03.java
@@ -172,8 +172,27 @@ public class StringUtils03 {
 	 * @return
 	 */
 	static String[] splitNotEmpty(String str, String separator) {
+		int count = 0;
 		
-		return null;
+		String[] split = split(str, separator);
+		
+		for (int i=0;i<split.length;i++) {
+			if (!StringUtils.isEmpty(split[i])) {
+				count++;
+			}
+		}
+		
+		String[] result = new String[count];
+		
+		for (int i=0, n=0;i<split.length;i++) {
+			if (StringUtils.isEmpty(split[i])) {
+				continue;
+			}
+			
+			result[n++] = split[i];
+		}		
+		
+		return result;
 	}
 	
 	/**
@@ -189,9 +208,49 @@ public class StringUtils03 {
 	 * @return
 	 */
 	static String[] splitNotBlank(String str, String separator) {
+		int count = 0;
+		
+		String[] split = split(str, separator);
+		
+		for (int i=0;i<split.length;i++) {
+			if (!StringUtils.isBlank(split[i])) {
+				count++;
+			}
+		}
+		
+		String[] result = new String[count];
+		
+		for (int i=0, n=0;i<split.length;i++) {
+			if (StringUtils.isBlank(split[i])) {
+				continue;
+			}
+			
+			result[n++] = split[i];
+		}		
+		
+		return result;
+	}
+	
+	
+	/**
+	 * 입력받은 문자열을 구분자를 기준으로 분리한 문자열의
+	 * 배열 중 빈문자열을 제외하고, 빈문자열이 아닌 문자열의
+	 * 앞뒤 공백도 제거한 문자열의 배열을 반환합니다.
+	 * 
+	 * 예1: splitNotBlank("hello, world", ",") -> ["hello", "world"]
+	 * 예2: splitNotBlank("5,4,3,2,1", ",") -> ["5", "4", "3", "2", "1"]
+	 * 예3: splitNotBlank("이상윤|조영욱|| ||", "||") -> ["이상윤|조영욱"]
+	 * 
+	 * @param str
+	 * @param separator
+	 * @return
+	 */
+	static String[] splitNotBlankAndTrim(String str, String separator) {
 		
 		return null;
 	}
+	
+	
 	
 	
 	
