e892ad98d58c18c6c94d73135d340a1c982806d7
5000 <5000@hb5000>
2016-12-02 10:40:46 +0900

    splitNotEmpty

--- src/com/hanbit/ex01/Tester.java
+++ src/com/hanbit/ex01/Tester.java
@@ -7,37 +7,19 @@ public class Tester {
 	public static void main(String[] args) {
 		
 		long time = System.nanoTime();
-		System.out.println("[" + StringUtils01.trim("   a  b  c   ") + "]");
+		printArray(StringUtils03.split("|||이 상 윤|조영욱|  ||", '|'));
 		System.out.println((System.nanoTime() - time) + "ns");
-		
-		time = System.nanoTime(); 
-		System.out.println("[" + StringUtils01.trimFast("   a  b  c   ") + "]");
-		System.out.println((System.nanoTime() - time) + "ns");
-		
-		time = System.nanoTime(); 
-		System.out.println("[" + "   a  b  c   ".trim() + "]");
-		System.out.println((System.nanoTime() - time) + "ns");
-		
-		System.out.println("[" + StringUtils01.leftTrim("   a  b  c   ") + "]");
-		System.out.println("[" + StringUtils01.rightTrim("   a  b  c   ") + "]");
-		System.out.println("[" + StringUtils01.removeWhitespace("   a  b  c   ") + "]");
-		
-		time = System.nanoTime();
-		System.out.println(StringUtils01.leftPad("11", 4, '0'));
-		System.out.println((System.nanoTime() - time) + "ns");
-		
-		time = System.nanoTime();
-		System.out.println(StringUtils01.rightPad("11", 4, '0'));
-		System.out.println((System.nanoTime() - time) + "ns");
-
-		time = System.nanoTime();
-		System.out.println(StringUtils01.repeat(4, '0'));
-		System.out.println((System.nanoTime() - time) + "ns");
-		
-		System.out.println("after:" + StringUtils02.substringAfter("banana", "banan"));
-		System.out.println("before:" + StringUtils02.substringBefore("banana", "B"));
-		
-		System.out.println("between:" + StringUtils02.substringBetween("char ch = 'z';", "'"));
+	}
+	
+	/**
+	 * 입력받은 배열을 한줄씩 출력합니다.
+	 * 
+	 * @param array
+	 */
+	static void printArray(String[] array) {
+		for (int i=0;i<array.length;i++) {
+			System.out.println(array[i]);
+		}
 	}
 	
 }
