3020859217fae7dc09a1be5c50b9519fbcae002f
5000 <5000@hb5000>
2016-12-02 16:58:24 +0900

    generatePrintln

--- src/com/hanbit/ex01/JavaCodeGenerator.java
+++ src/com/hanbit/ex01/JavaCodeGenerator.java
@@ -19,17 +19,54 @@ public class JavaCodeGenerator {
 		 *
 		 * }
 		 */
-		System.out.println(generateSourceCode("Hello, Java", new String[] {"print"}));
+		//System.out.println(generateSourceCode("Hello, Java", 5, "takeRest"));
+		
+		String template = "System.out.println([var]);";
+		
+		/*
+		 * 다음과 같이 출력하시오.
+		 * 
+		 * System.out.println("Hello, World");
+		 */
+		System.out.println(generatePrintln(template, "Hello, World"));
 	}
 
-	static String generateSourceCode(String string) {
+	static String generateSourceCode(String string, int hour, String... functions) {
 		String code = "public class JavaCodeGenerator {\n\n";
 		code += "\tpublic static void main(String[] args) {\n";
 		code += "\t\tSystem.out.println(\"" + string + "\");\n";
+		
+		for (int i=0;i<functions.length;i++) {
+			code += "\t\t" + functions[i] +"(" + hour + ");\n";
+		}
+		
 		code += "\t}\n\n";
-		code += "}";
+		
+		for (int i=0;i<functions.length;i++) {
+			code += "\tstatic void " + functions[i] + "(int hour) {\n";
+			code += "\t\tSystem.out.println(hour + \"시까지 쉴게요.\");\n";
+			code += "\t}\n\n";
+		}
+		
+		code += "\n}";
 		
 		return code;
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
